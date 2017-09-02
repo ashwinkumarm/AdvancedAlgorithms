13ba3dd1ddf8b868c09a065ba3fa589a76c5d75a
Arun Kumar Balasubramani <axb160631@utdallas.edu>
2017-09-01 19:07:55 -0500

    Removed commented code

--- sp1_q2_GraphDiameter/CC.java
+++ sp1_q2_GraphDiameter/CC.java
@@ -1,11 +1,7 @@
 package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.sp1_q2_GraphDiameter;
 
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.util.Scanner;
+public class CC {
 
-
-public class CC { 
 	class CCVertex {
 		Graph.Vertex element;
 		boolean seen;
@@ -16,6 +12,7 @@ public class CC {
 		    parent = null;
 		}
     }
+
     // Algorithm uses a parallel array for storing information about vertices
     CCVertex[] ccVertex;
     Graph g;
@@ -23,31 +20,9 @@ public class CC {
     public CC(Graph g) {
 	this.g = g;
 	ccVertex = new CCVertex[g.size()];
-	for(Graph.Vertex u: g) 
+	for(Graph.Vertex u: g)
 		{ ccVertex[u.name] = new CCVertex(u); }
     }
-
-    // Main algorithm for finding the number of connected components of g using DFS
-//    int findCC() {
-//	int cno = 0;
-//	for(Graph.Vertex u: g) {
-//	    if(!seen(u)) {
-//		cno++;
-//		dfsVisit(u, cno);
-//	    }
-//	}
-//	return cno;
-//    }
-//
-//    void dfsVisit(Graph.Vertex u, int cno) {
-//	visit(u, cno);
-//	for(Graph.Edge e: u) {
-//	    Graph.Vertex v = e.otherEnd(u);
-//	    if(!seen(v)) {
-//		dfsVisit(v, cno);
-//	    }
-//	}
-//    }
 
     boolean seen(Graph.Vertex u) {
 	CCVertex ccu = getCCVertex(u);
@@ -58,7 +33,6 @@ public class CC {
     void visit(Graph.Vertex u, int cno) {
 	CCVertex ccu = getCCVertex(u);
 	ccu.seen = true;
-	//ccu.parent = cno;
     }
 
     // From Vertex to CCVertex (ugly)
@@ -70,33 +44,4 @@ public class CC {
     Graph.Vertex getVertex(CCVertex c) {
 	return c.element;
     }
-
-
-
-//    public static void main(String[] args) throws FileNotFoundException {
-//	int evens = 0;
-//	Scanner in;
-//        if (args.length > 0) {
-//            File inputFile = new File(args[0]);
-//            in = new Scanner(inputFile);
-//        } else {
-//            in = new Scanner(System.in);
-//        }
-//	Graph g = Graph.readGraph(in);
-//	CC cc = new CC(g);
-////	int nc = cc.findCC();
-////
-////	System.out.println("Input Graph has " + nc + " components:");
-////	for(Graph.Vertex u: g) {
-////	    System.out.print(u + " [ " + cc.getCCVertex(u).cno + " ] :");
-////	    for(Graph.Edge e: u.adj) {
-////		Graph.Vertex v = e.otherEnd(u);
-////		System.out.print(e + " ");
-////	    }
-////	    System.out.println();
-////	}
-//    }
-	
-	
-
 }
--- sp1_q2_GraphDiameter/GraphDiameter.java
+++ sp1_q2_GraphDiameter/GraphDiameter.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 /**
  * Class GraphDiameter
- * 
- * <P> This class forms the graph and finds the longest path by 
+ *
+ * <P> This class forms the graph and finds the longest path by
  * running bfs twice.
- * 
+ *
  * @author Ashwin, Arun, Deepak, Haritha
  *
  */
@@ -18,11 +18,11 @@ public class GraphDiameter {
 
 	/**
 	 * Finds the longest path by running bfs.
-	 * 
+	 *
 	 * @param g graph
 	 * */
 	static LinkedList<Graph.Vertex> diameter(Graph g) {
-		
+
 		LinkedList<Graph.Vertex> initialPath = BreadthFirstSearch.bfs(g, g.v[0]); // runs  bfs for the first node in the vertices list
 		LinkedList<Graph.Vertex> finalPath = BreadthFirstSearch.bfs(g, initialPath.get(0)); // runs bfs for the farthest node from the first vertice
 		return finalPath;
@@ -41,7 +41,7 @@ public class GraphDiameter {
 			in = new Scanner(System.in);
 		}
 		Graph graph = Graph.readGraph(in); // passes the scanner object to graph class to construct the graph
-		
+
 		// if graph is not empty we call the diameter method
 		if(graph.v.length > 0 && graph.n > 0){
 		LinkedList<Graph.Vertex> longestPath = diameter(graph);
