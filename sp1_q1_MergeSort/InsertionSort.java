0d0836b433ff91f5bba312d8a68549e907ffb7c7
Deepak Shanmugam <deepaksrd94@gmail.com>
2017-09-03 11:46:36 -0500

    Changes to insertion sort according to CLRS

--- sp1_q1_MergeSort/InsertionSort.java
+++ sp1_q1_MergeSort/InsertionSort.java
@@ -4,7 +4,8 @@ package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.sp1
  * Class InsertionSort
  * 
  * <P>
- * This class performs insertion sort on a generic array of size ranging from 1M - 16M
+ * This class performs insertion sort on a generic array of size ranging from 1M
+ * - 16M
  * 
  * @author Ashwin, Arun, Deepak, Haritha
  *
@@ -19,13 +20,13 @@ public class InsertionSort {
 	 */
 	public static <T extends Comparable<? super T>> void nSquareSort(T[] arr) {
 		for (int i = 1; i < arr.length; i++) {
-			for (int j = i; j > 0; j--) {
-				if (arr[j].compareTo(arr[j - 1]) < 0) {
-					T tmp = arr[j];
-					arr[j] = arr[j - 1];
-					arr[j - 1] = tmp;
-				}
+			T key = arr[i];
+			int j = i - 1;
+			while (j > 0 && arr[j].compareTo(key) > 0) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
 			}
+			arr[j + 1] = key;
 		}
 	}
 }
