d46135f641c5ca3114c5a17f223cbc4883d0317e
Deepak Shanmugam <deepaksrd94@gmail.com>
2017-09-06 11:12:51 -0500

    SP2 q8 another version
    ShuntingYard Algorithm in another version with enum

--- sp2_q8_ShuntingYard/ShuntingYard1.java
+++ sp2_q8_ShuntingYard/ShuntingYard1.java
@@ -3,30 +3,20 @@ package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.sp2
 import java.util.HashMap;
 import java.util.Stack;
 
+public class ShuntingYard1 {
 
-public class ShuntingYard {
+	enum Operator {
+		ADD(1, 1), SUBTRACT(1, 1), MULTIPLY(2, 1), DIVIDE(2, 1), MODULUS(2, 1), EXPONENT(3, 2), FACTORIAL(4, 0);
 
-	private final static HashMap<String, int[]> operatorMap = new HashMap<String, int[]>();
+		final int precedence, associativity;
 
-	static class Operator {
-		String symbol;
-
-		public Operator(String symbol, int associativity, int precedence) {
-			this.symbol = symbol;
-			int[] tmp = { associativity, precedence };
-			operatorMap.put(this.symbol, tmp);
+		private Operator(int precedence, int associativity) {
+			this.precedence = precedence;
+			this.associativity = associativity;
 		}
 	}
 
-	private static void initializeOperators() {
-		new Operator("+", 1, 1);
-		new Operator("-", 1, 1);
-		new Operator("*", 1, 2);
-		new Operator("/", 1, 2);
-		new Operator("%", 1, 2);
-		new Operator("^", 2, 3);
-		new Operator("!", 0, 4);
-	}
+	private static HashMap<String, Operator> operatorMap = new HashMap<String, Operator>();
 
 	public static String infixToPostfix(String infix) {
 		StringBuilder postfix = new StringBuilder();
@@ -37,8 +27,9 @@ public class ShuntingYard {
 			if (operatorMap.containsKey(ch)) {
 				while (!st.isEmpty()) {
 					String topOperator = st.peek();
-					if ((!topOperator.equals("(") && isleftAssociative(ch) && comparePrecedence(ch, topOperator) <= 0)
-							|| (!topOperator.equals("(") && !isleftAssociative(ch)
+					if ((operatorMap.containsKey(topOperator) && isleftAssociative(ch)
+							&& comparePrecedence(ch, topOperator) <= 0)
+							|| (operatorMap.containsKey(topOperator) && !isleftAssociative(ch)
 									&& comparePrecedence(ch, topOperator) < 0))
 						postfix.append(st.pop());
 					else
@@ -66,19 +57,30 @@ public class ShuntingYard {
 		return postfix.toString();
 	}
 
-	private static boolean isleftAssociative(String op) {
-		return operatorMap.get(op)[0] == 1;
+	private static boolean isleftAssociative(String ch) {
+		return operatorMap.get(ch).associativity == 1;
 	}
 
 	private static int comparePrecedence(String op1, String op2) {
-		int op1Precedence = operatorMap.get(op1)[1];
-		int op2Precedence = operatorMap.get(op2)[1];
+		int op1Precedence = operatorMap.get(op1).precedence;
+		int op2Precedence = operatorMap.get(op2).precedence;
 		if (op1Precedence == op2Precedence)
 			return 0;
 		else if (op1Precedence < op2Precedence)
 			return -1;
 		else
 			return 1;
+	}
+
+	private static void initializeOperators() {
+		operatorMap.put("+", Operator.ADD);
+		operatorMap.put("-", Operator.SUBTRACT);
+		operatorMap.put("*", Operator.MULTIPLY);
+		operatorMap.put("/", Operator.DIVIDE);
+		operatorMap.put("%", Operator.MODULUS);
+		operatorMap.put("^", Operator.EXPONENT);
+		operatorMap.put("!", Operator.FACTORIAL);
+
 	}
 
 	public static void main(String args[]) {
