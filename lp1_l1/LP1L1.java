99883cab764aa1a0f1610a16d369349655457666
Deepak Shanmugam <deepaksrd94@gmail.com>
2017-09-11 14:22:14 -0500

    LP1L1 Base split changes

--- lp1_l1/LP1L1.java
+++ lp1_l1/LP1L1.java
@@ -2,12 +2,14 @@ package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.lp1
 
 public class LP1L1 {
 	public static void main(String[] args) {
-		Num x = new Num(99124234);
-		Num y = new Num("8675");
-		Num z = Num.add(x, y);
-		System.out.println(z);
+		Num x = new Num(991242349);
+		System.out.println(x.ll);
+		Num y = new Num("867576876924692");
+		System.out.println(y.ll);
+		Num z = Num.add(x, y); //Ans should be 867577868167041
+		//System.out.println(z);
 		Num a = Num.power(x, 8);
-		System.out.println(a);
-		z.printList();
+		//System.out.println(a);
+		//z.printList();
 	}
 }
--- lp1_l1/Num.java
+++ lp1_l1/Num.java
@@ -7,14 +7,15 @@ package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.lp1
 import java.util.LinkedList;
 
 public class Num implements Comparable<Num> {
-
-	static long defaultBase = 10; // This can be changed to what you want it to
-									// be.
+	// Long can store only upto 9 digits
+	// So the base has to be set in a way such that it can divide upto 9 digits
+	static long defaultBase = 999999999; // This can be changed to what you want it to be.
 	long base = defaultBase; // Change as needed
 	boolean isNegative = false;
+	long baseLength = (long) (Math.log10(base) + 1);
 	/* Start of Level 1 */
 	LinkedList<Long> ll = new LinkedList<>();
-	LinkedList<Integer> outList = new LinkedList<Integer>();
+	LinkedList<Long> outList = new LinkedList<>();
 
 	/**
 	 * Converts the string into a linked list
@@ -22,8 +23,7 @@ public class Num implements Comparable<Num> {
 	 * @param s
 	 */
 	Num(String s) {
-		int cursor = 0, numDigits;
-		long baseLength = (long) (Math.log10(base) + 1);
+		int cursor = 0;
 		final int len = s.length();
 
 		if (len == 0)
@@ -40,30 +40,31 @@ public class Num implements Comparable<Num> {
 			throw new NumberFormatException("Zero length BigInteger");
 
 		// Skip leading zeros and compute number of digits in magnitude
-		while (cursor < len && Character.digit(s.charAt(cursor), (int) defaultBase) == 0) {
-			cursor++;
-		}
+		// while (cursor < len && Character.digit(s.charAt(cursor), (int) defaultBase)
+		// == 0) {
+		// cursor++;
+		// }
+		//
+		// if (cursor == len) {
+		// ll.add(0L);
+		// return;
+		// }
 
-		if (cursor == len) {
-			ll.add(0L);
-			return;
-		}
-
-		numDigits = len - cursor;
+		// numDigits = len - cursor;
 
 		String group;
 		long groupVal = 0;
-		while (cursor+baseLength < len) {
-			group = s.substring(cursor, (int) (cursor + baseLength));
+		int reversePtr = len;
+		while (reversePtr - baseLength >= cursor) {
+			group = s.substring((int) (reversePtr - baseLength), reversePtr);
 			groupVal = Long.parseLong(group);
-			if (groupVal < base) {
-				cursor += baseLength;
-				ll.add(groupVal);
-			} else {
-				group = s.substring(cursor, (int) (cursor += baseLength - 1));
-				groupVal = Long.parseLong(group);
-				ll.add(groupVal);
-			}
+			reversePtr -= baseLength;
+			ll.add(groupVal);
+		}
+		if (reversePtr != cursor) {
+			group = s.substring(cursor, reversePtr);
+			groupVal = Long.parseLong(group);
+			ll.add(groupVal);
 		}
 	}
 
@@ -73,9 +74,26 @@ public class Num implements Comparable<Num> {
 	 * @param x
 	 */
 	Num(long x) {
-		while (x > 0) {
-			ll.add(x % base);
-			x /= base;
+		int cursor = 0;
+		int len = (int) (Math.log10(x) + 1);
+
+		if (len == 0)
+			throw new NumberFormatException("Zero length LongInteger");
+
+		String s = Long.toString(x);
+		String group;
+		long groupVal = 0;
+		int reversePtr = len;
+		while (reversePtr - baseLength >= cursor) {
+			group = s.substring((int) (reversePtr - baseLength), reversePtr);
+			groupVal = Long.parseLong(group);
+			reversePtr -= baseLength;
+			ll.add(groupVal);
+		}
+		if (reversePtr != cursor) {
+			group = s.substring(cursor, reversePtr);
+			groupVal = Long.parseLong(group);
+			ll.add(groupVal);
 		}
 	}
 
