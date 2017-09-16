7c6cc77e76444c8039d4c36148f88be06bbec81a
Arun Kumar Balasubramani <axb160631@utdallas.edu>
2017-09-16 00:49:12 -0500

    Commit for divide by 2

--- lp1_l1/LP1L1.java
+++ lp1_l1/LP1L1.java
@@ -2,45 +2,28 @@ package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.lp1
 
 public class LP1L1 {
 	public static void main(String[] args) {
-		//BigInteger b = new BigInteger();
-		Num x = new Num(15);
+		// BigInteger b = new BigInteger();
+		Num x = new Num(40);
 		Num y = new Num(5);
-		System.out.println(Num.product(x, y));
-		//Num y1 = new Num("-867576876924692");
-		//System.out.println(y1.digits);
-		//Num z = Num.add(x,y); //Ans should be 867577868167041
-		Num s = Num.add(x,y); //Ans shld be 867575885682343
+		System.out.println(x);
+		// Num y1 = new Num("-867576876924692");
+		// System.out.println(y1.digits);
+		// Num z = Num.add(x,y); //Ans should be 867577868167041
+		Num s = Num.add(x, y); // Ans shld be 867575885682343
 		System.out.println(s);
 
-		System.out.println(Num.divide(x, y));
-		//long z = Num.product(x, y);
-		//System.out.println(z);
-		//Num a = Num.power(x, 8);
-		//System.out.println(a);
-		//z.printList();
+		System.out.println(Num.divideByTwo(x));
 		/**
-		 * Test Input
-		 * add
-		 * 991242349, -867576876924692
-		 * -867575885682343
-		 * -991242349, 867576876924692
-		 * 867575885682343
-		 * -991242349, -867576876924692
-		 * -867577868167041
-		 * 991242349, 867576876924692
+		 * Test Input add 991242349, -867576876924692 -867575885682343
+		 * -991242349, 867576876924692 867575885682343 -991242349,
+		 * -867576876924692 -867577868167041 991242349, 867576876924692
 		 * 867577868167041
 		 *
-		 * subtract
-		 * 991242349, 867576876924692
-		 * -867575885682343
-		 * 991242349, -867576876924692
-		 * 867577868167041
-		 * -991242349, 867576876924692
-		 * -867577868167041
-		 * -991242349, -867576876924692
-		 * 867575885682343
+		 * subtract 991242349, 867576876924692 -867575885682343 991242349,
+		 * -867576876924692 867577868167041 -991242349, 867576876924692
+		 * -867577868167041 -991242349, -867576876924692 867575885682343
 		 *
-		 * **/
+		 **/
 
 	}
 }
