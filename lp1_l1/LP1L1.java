65c69db93078a55618fc5dedc293bb222f51a31c
ashwinkumarm <ashbushster@gmail.com>
2017-09-16 02:11:24 -0500

    Karatsuba Multiplication Ver 2.0

--- lp1_l1/LP1L1.java
+++ lp1_l1/LP1L1.java
@@ -4,15 +4,16 @@ public class LP1L1 {
 	public static void main(String[] args) {
 		// BigInteger b = new BigInteger();
 		Num x = new Num(40);
-		Num y = new Num(5);
-		System.out.println(x);
-		// Num y1 = new Num("-867576876924692");
+		Num y = new Num(50);
+		//System.out.println(x);
+		//Num y1 = new Num("-867576876924692");
 		// System.out.println(y1.digits);
 		// Num z = Num.add(x,y); //Ans should be 867577868167041
-		Num s = Num.add(x, y); // Ans shld be 867575885682343
-		System.out.println(s);
+		//Num s = Num.add(x, y); // Ans shld be 867575885682343
+		Num d = Num.product(x, y);
+		System.out.println(d);
 
-		System.out.println(Num.divideByTwo(x));
+		//System.out.println(Num.divideByTwo(x));
 		/**
 		 * Test Input add 991242349, -867576876924692 -867575885682343
 		 * -991242349, 867576876924692 867575885682343 -991242349,
