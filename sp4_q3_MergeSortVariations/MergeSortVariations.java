20b5eedf3ca585da275feb190a7b620271838692
Deepak Shanmugam <deepaksrd94@gmail.com>
2017-09-24 11:02:39 -0500

    SP4 Q3 Added comments

--- sp4_q3_MergeSortVariations/MergeSortVariations.java
+++ sp4_q3_MergeSortVariations/MergeSortVariations.java
@@ -6,22 +6,50 @@ import java.util.Scanner;
 
 import cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.sp1_q1_MergeSort.Timer;
 
+/**
+ * This class performs 4 differrent versions of merge sort
+ * 
+ * @author Ashwin, Arun, Deepak, Haritha
+ *
+ */
 public class MergeSortVariations {
 	static Integer inf = Integer.MAX_VALUE;
 
-	static void mergeSortAlgorithm1(int[] A) {
-		mergeSortAlgorithm1(A, 0, A.length - 1);
+	/**
+	 * Performs the merge sort algorithm by creating two tmp arrays in the merge
+	 * phase
+	 * 
+	 * @param A
+	 */
+	static void mergeSortVersion1(int[] A) {
+		mergeSortVersion1(A, 0, A.length - 1);
 	}
 
-	private static void mergeSortAlgorithm1(int[] A, int l, int r) {
+	/**
+	 * Recursively divides the array and performs the merge operation by sorting the
+	 * arrays
+	 * 
+	 * @param A
+	 * @param l
+	 * @param r
+	 */
+	private static void mergeSortVersion1(int[] A, int l, int r) {
 		if (l < r) {
 			int mid = (l + r) / 2;
-			mergeSortAlgorithm1(A, l, mid);
-			mergeSortAlgorithm1(A, mid + 1, r);
+			mergeSortVersion1(A, l, mid);
+			mergeSortVersion1(A, mid + 1, r);
 			merge1(A, l, mid, r);
 		}
 	}
 
+	/**
+	 * Merge method sorts the given array with the help of two tmp arrays(L and R)
+	 * 
+	 * @param A
+	 * @param l
+	 * @param mid
+	 * @param r
+	 */
 	private static void merge1(int[] A, int l, int mid, int r) {
 		int[] L = new int[mid - l + 2];
 		int[] R = new int[r - mid + 1];
@@ -38,19 +66,44 @@ public class MergeSortVariations {
 		}
 	}
 
-	static void mergeSortAlgorithm2(int[] A) {
-		mergeSortAlgorithm2(A, 0, A.length - 1);
+	/**
+	 * Performs the merge sort algorithm by passing the tmp array as an argument to
+	 * the merge phase
+	 * 
+	 * @param A
+	 */
+	static void mergeSortVersion2(int[] A) {
+		mergeSortVersion2(A, 0, A.length - 1);
 	}
 
-	private static void mergeSortAlgorithm2(int[] A, int l, int r) {
+	/**
+	 * Recursively divides the array and performs the merge operation by sorting the
+	 * arrays
+	 * 
+	 * @param A
+	 * @param l
+	 * @param r
+	 */
+	private static void mergeSortVersion2(int[] A, int l, int r) {
 		if (l < r) {
 			int mid = (l + r) / 2;
-			mergeSortAlgorithm2(A, l, mid);
-			mergeSortAlgorithm2(A, mid + 1, r);
+			mergeSortVersion2(A, l, mid);
+			mergeSortVersion2(A, mid + 1, r);
 			merge2(A, new int[A.length], l, mid, r);
 		}
 	}
 
+	/**
+	 * 
+	 * Merge method sorts the given array with the help of one tmp arrays passed as
+	 * an argument
+	 * 
+	 * @param A
+	 * @param tmp
+	 * @param l
+	 * @param mid
+	 * @param r
+	 */
 	private static void merge2(int[] A, int[] tmp, int l, int mid, int r) {
 		System.arraycopy(A, l, tmp, l, r - l + 1);
 		int i = l, j = mid + 1;
@@ -62,21 +115,42 @@ public class MergeSortVariations {
 		}
 	}
 
-	static void mergeSortAlgorithm3(int A[]) {
-		mergeSortAlgorithm3(A, 0, A.length - 1);
+	/**
+	 * Performs the merge sort algorithm by performing the insertion sort for the
+	 * array size less than some threshold else performs the merge sort
+	 * 
+	 * @param A
+	 */
+	static void mergeSortVersion3(int A[]) {
+		mergeSortVersion3(A, 0, A.length - 1);
 	}
 
-	private static void mergeSortAlgorithm3(int[] A, int l, int r) {
-		if ((r - l) < 5) {
+	/**
+	 * Recursively divides the array and performs the insertion sort for some
+	 * threshold or else merge operation by sorting the arrays
+	 * 
+	 * @param A
+	 * @param l
+	 * @param r
+	 */
+	private static void mergeSortVersion3(int[] A, int l, int r) {
+		if ((r - l) < 50) {
 			insertionSort(A, l, r);
 		} else {
 			int mid = (l + r) / 2;
-			mergeSortAlgorithm3(A, l, mid);
-			mergeSortAlgorithm3(A, mid + 1, r);
+			mergeSortVersion3(A, l, mid);
+			mergeSortVersion3(A, mid + 1, r);
 			merge3(A, new int[A.length], l, mid, r);
 		}
 	}
 
+	/**
+	 * Performs the insertion sort on the given array
+	 * 
+	 * @param A
+	 * @param l
+	 * @param r
+	 */
 	private static void insertionSort(int[] A, int l, int r) {
 		int i = l + 1;
 		int key, j;
@@ -92,6 +166,16 @@ public class MergeSortVariations {
 		}
 	}
 
+	/**
+	 * Merge method sorts the given array with the help of one tmp array passed as
+	 * an argument
+	 * 
+	 * @param A
+	 * @param tmp
+	 * @param l
+	 * @param mid
+	 * @param r
+	 */
 	private static void merge3(int[] A, int[] tmp, int l, int mid, int r) {
 		System.arraycopy(A, l, tmp, l, r - l + 1);
 		int i = l, j = mid + 1;
@@ -103,23 +187,49 @@ public class MergeSortVariations {
 		}
 	}
 
-	static void mergeSortAlgorithm4(int A[]) {
+	/**
+	 * Performs the merge sort algorithm by performing the insertion sort for the
+	 * array size less than some threshold else performs the merge sort without
+	 * copying the input array to tmp array
+	 * 
+	 * @param A
+	 */
+	static void mergeSortVersion4(int A[]) {
 		int B[] = new int[A.length];
 		System.arraycopy(A, 0, B, 0, A.length);
-		mergeSortAlgorithm4(A, B, 0, A.length - 1);
+		mergeSortVersion4(A, B, 0, A.length - 1);
 	}
 
-	private static void mergeSortAlgorithm4(int[] A, int[] B, int l, int r) {
+	/**
+	 * Recursively divides the array and performs the insertion sort for some
+	 * threshold or else merge operation by sorting the arrays
+	 * 
+	 * @param A
+	 * @param B
+	 * @param l
+	 * @param r
+	 */
+	private static void mergeSortVersion4(int[] A, int[] B, int l, int r) {
 		if ((r - l) < 5) {
 			insertionSort(A, l, r);
 		} else {
 			int mid = (l + r) / 2;
-			mergeSortAlgorithm4(B, A, l, mid);
-			mergeSortAlgorithm4(B, A, mid + 1, r);
+			mergeSortVersion4(B, A, l, mid);
+			mergeSortVersion4(B, A, mid + 1, r);
 			merge4(B, A, l, mid, r);
 		}
 	}
 
+	/**
+	 * Merge method sorts the given array without copying the input array to tmp
+	 * array
+	 * 
+	 * @param A
+	 * @param B
+	 * @param l
+	 * @param mid
+	 * @param r
+	 */
 	private static void merge4(int[] A, int[] B, int l, int mid, int r) {
 		int i = l, j = mid + 1;
 		for (int k = l; k <= r; k++) {
@@ -143,35 +253,39 @@ public class MergeSortVariations {
 		int size = in.nextInt();
 		int[] arr = new int[size];
 		for (int i = 0; i < size; i++) {
-			arr[i] = rand.nextInt(500);
+			arr[i] = rand.nextInt(1000);
 		}
 		in.close();
 		int[] inputArray = arr;
 		Timer t = new Timer();
-		mergeSortAlgorithm1(inputArray);
+		mergeSortVersion1(inputArray);
 		System.out.println("\nTime and Memory taken by MergeSort Type1:\n" + t.end());
-		/*for(int i=0;i<size;i++)
-			System.out.print(inputArray[i]+" ");*/
-		
+		/*
+		 * for(int i=0;i<size;i++) System.out.print(inputArray[i]+" ");
+		 */
+
 		inputArray = arr;
 		t.start();
-		mergeSortAlgorithm2(inputArray);
+		mergeSortVersion2(inputArray);
 		System.out.println("\nTime and Memory taken by MergeSort Type2:\n" + t.end());
-		/*for(int i=0;i<size;i++)
-			System.out.print(inputArray[i]+" ");*/
-		
+		/*
+		 * for(int i=0;i<size;i++) System.out.print(inputArray[i]+" ");
+		 */
+
 		inputArray = arr;
 		t.start();
-		mergeSortAlgorithm3(inputArray);
+		mergeSortVersion3(inputArray);
 		System.out.println("\nTime and Memory taken by MergeSort Type3:\n" + t.end());
-		/*for(int i=0;i<size;i++)
-			System.out.print(inputArray[i]+" ");*/
-		
+		/*
+		 * for(int i=0;i<size;i++) System.out.print(inputArray[i]+" ");
+		 */
+
 		inputArray = arr;
 		t.start();
-		mergeSortAlgorithm4(inputArray);
+		mergeSortVersion4(inputArray);
 		System.out.println("\nTime and Memory taken by MergeSort Type4:\n" + t.end());
-		/*for(int i=0;i<size;i++)
-			System.out.print(inputArray[i]+" ");*/
+		/*
+		 * for(int i=0;i<size;i++) System.out.print(inputArray[i]+" ");
+		 */
 	}
 }
