eadb756deec8505b30ecea47dae26c6c2dfcf9f8
ashwinkumarm <ashbushster@gmail.com>
2017-10-05 01:56:25 -0500

    perfect power code

---
+++ sp6_q3_PerfectPowers/Triplet.java
@@ -0,0 +1,26 @@
+package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.sp6_q3_PerfectPowers;
+
+/**
+ * Triplet class for perfect power
+ *
+ * @author Ashwin, Arun, Deepak, Haritha
+ *
+ */
+public class Triplet implements Comparable<Triplet>{
+	
+	int a;
+	int b;
+	double value;
+	
+	Triplet(int a, int b, double value){
+		this.a = a;
+		this.b = b;
+		this.value = value;
+	}	
+	
+	@Override
+	public int compareTo(Triplet t) {
+		return  (int) (this.value - t.value);
+	}
+
+}
