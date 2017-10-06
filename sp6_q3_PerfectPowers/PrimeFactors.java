3dc43d95b0bdc996030326bca60532add145e51c
ashwinkumarm <ashbushster@gmail.com>
2017-10-06 03:22:56 -0500

    Prime Factor
    Not sure if its efficient

---
+++ sp6_q3_PerfectPowers/PrimeFactors.java
@@ -0,0 +1,45 @@
+package cs6301.g12.Implementation_of_Advanced_Data_Structures_and_Algorithms.sp6_q3_PerfectPowers;
+
+import java.util.PriorityQueue;
+
+/**
+ * Class to find prime factors
+ *
+ * @author Ashwin, Arun, Deepak, Haritha
+ *
+ */
+public class PrimeFactors {
+	
+	public static void primeFactors(int[] arr, int n) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+		for (int i : arr) {
+			pq.add(i);
+		}
+
+		int prev = 0;
+		while (!pq.isEmpty()) {
+			int curr = pq.poll();
+			// breaks the loop if greater than user given value or negative
+			if (curr >= n || curr <= 0) {
+				break;
+			}
+
+			if (curr != prev) {
+				System.out.println(curr);
+				prev = curr;
+			}
+
+			for (int i : arr) {
+				pq.add(curr * i);
+			}
+		}
+
+	}
+	
+	public static void main(String[] args){
+		int[] a = {3,5,7};
+		primeFactors(a,Integer.MAX_VALUE);
+	}
+
+}
