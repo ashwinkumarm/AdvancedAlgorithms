825336be870f81bcb922b46932511b6b0cf306ad
Arun Kumar Balasubramani <axb160631@utdallas.edu>
2017-11-18 18:09:06 -0600

    Type cast fix to sp9 q2

--- sp9_q2_ExactlyOnce/ExactlyOnce.java
+++ sp9_q2_ExactlyOnce/ExactlyOnce.java
@@ -4,12 +4,13 @@ import java.util.HashMap;
 
 public class ExactlyOnce {
 
-	static <T> void display(T[] o){
+	static <T> void display(T[] o) {
 		for (T i : o) {
 			if (i != null)
 				System.out.println(i);
 		}
 	}
+
 	static <T extends Comparable<? super T>> T[] exactlyOnce(T[] A) {
 		T[] output = (T[]) new Comparable[A.length];
 
@@ -22,18 +23,11 @@ public class ExactlyOnce {
 				output[valueWithIndexPos.get(A[i])] = null;
 			}
 		}
-
-		for (T i : output) {
-			if (i != null)
-				System.out.println(i);
-		}
 		return output;
 	}
-		
 
 	public static void main(String[] args) {
 		Integer[] A = { 6, 3, 4, 5, 3, 5 };
-		exactlyOnce(A);
-		//display(exactlyOnce(A));
+		display((Comparable[]) exactlyOnce(A));
 	}
 }
